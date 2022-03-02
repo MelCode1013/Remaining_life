f57a94bd898d19bfee9ab52d1d6d87a4dbba25bd
Melinna Bayona <96249341+MelCode1013@users.noreply.github.com>
2022-03-02 01:03:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+years_left = 90 - int(age)
+days_left = 365 * years_left
+weeks_left = 52 * years_left
+months_left = 12 * years_left
+
+print(f"If you lived to be 90, then you have {days_left} days, {weeks_left} weeks, and {months_left} months left.")
+
+
+
+
+
+
+
+
